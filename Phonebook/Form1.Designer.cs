c00f186d2acf91293ce5d24211fd7437d9a62468
Colin Gillette <colingillette35@gmail.com>
2018-05-10 21:34:27 -0500

    all elements implemented

--- Phonebook/Form1.Designer.cs
+++ Phonebook/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.modeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -35,10 +36,20 @@
             this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.saveButton = new System.Windows.Forms.Button();
-            this.Name1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.database1DataSet = new Phonebook.Database1DataSet();
+            this.tableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tableTableAdapter = new Phonebook.Database1DataSetTableAdapters.TableTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dbName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dbNumber = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Number = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Name1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
@@ -64,21 +75,21 @@
             // viewToolStripMenuItem
             // 
             this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
-            this.viewToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.viewToolStripMenuItem.Text = "View";
             this.viewToolStripMenuItem.Click += new System.EventHandler(this.viewToolStripMenuItem_Click);
             // 
             // insertToolStripMenuItem
             // 
             this.insertToolStripMenuItem.Name = "insertToolStripMenuItem";
-            this.insertToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.insertToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.insertToolStripMenuItem.Text = "Insert";
             this.insertToolStripMenuItem.Click += new System.EventHandler(this.insertToolStripMenuItem_Click);
             // 
             // editToolStripMenuItem
             // 
             this.editToolStripMenuItem.Name = "editToolStripMenuItem";
-            this.editToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.editToolStripMenuItem.Text = "Edit";
             this.editToolStripMenuItem.Click += new System.EventHandler(this.editToolStripMenuItem_Click);
             // 
@@ -87,19 +98,25 @@
             this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Name1,
-            this.Number});
-            this.dataGridView1.Location = new System.Drawing.Point(25, 58);
+            this.idDataGridViewTextBoxColumn,
+            this.dbName,
+            this.dbNumber,
+            this.Number,
+            this.Name1});
+            this.dataGridView1.DataSource = this.tableBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 89);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(317, 363);
+            this.dataGridView1.Size = new System.Drawing.Size(317, 332);
             this.dataGridView1.TabIndex = 1;
             // 
             // saveButton
             // 
             this.saveButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.saveButton.Enabled = false;
             this.saveButton.Location = new System.Drawing.Point(134, 441);
             this.saveButton.Name = "saveButton";
             this.saveButton.Size = new System.Drawing.Size(90, 35);
@@ -108,30 +125,89 @@
             this.saveButton.UseVisualStyleBackColor = true;
             this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
             // 
-            // Name1
+            // database1DataSet
             // 
-            this.Name1.HeaderText = "Name";
-            this.Name1.Name = "Name1";
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tableBindingSource
+            // 
+            this.tableBindingSource.DataMember = "Table";
+            this.tableBindingSource.DataSource = this.database1DataSet;
+            // 
+            // tableTableAdapter
+            // 
+            this.tableTableAdapter.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // dbName
+            // 
+            this.dbName.DataPropertyName = "Name";
+            this.dbName.HeaderText = "Name";
+            this.dbName.Name = "dbName";
+            // 
+            // dbNumber
+            // 
+            this.dbNumber.DataPropertyName = "Number";
+            this.dbNumber.HeaderText = "Number";
+            this.dbNumber.Name = "dbNumber";
             // 
             // Number
             // 
             this.Number.HeaderText = "Number";
             this.Number.Name = "Number";
+            this.Number.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Number.Visible = false;
+            // 
+            // Name1
+            // 
+            this.Name1.HeaderText = "Name";
+            this.Name1.Name = "Name1";
+            this.Name1.Visible = false;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.searchButton.Location = new System.Drawing.Point(261, 40);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(81, 27);
+            this.searchButton.TabIndex = 3;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.textBox1.Location = new System.Drawing.Point(25, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(199, 20);
+            this.textBox1.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(378, 488);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.searchButton);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Phonebook";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -146,8 +222,16 @@
         private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button saveButton;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Name1;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource tableBindingSource;
+        private Database1DataSetTableAdapters.TableTableAdapter tableTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dbName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dbNumber;
         private System.Windows.Forms.DataGridViewTextBoxColumn Number;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name1;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Phonebook/Form1.cs
+++ Phonebook/Form1.cs
@@ -12,6 +12,8 @@ namespace Phonebook
 {
     public partial class Form1 : Form
     {
+        DataClasses1DataContext db = new DataClasses1DataContext();
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +21,26 @@ namespace Phonebook
 
         private void viewToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            db = new DataClasses1DataContext();
 
+            dataGridView1.DataSource = db.Phones;
+            dataGridView1.Columns["dbName"].Visible = true;
+            dataGridView1.Columns["dbNumber"].Visible = true;
+            dataGridView1.Columns["Name1"].Visible = false;
+            dataGridView1.Columns["Number"].Visible = false;
+            saveButton.Enabled = false;
         }
 
         private void insertToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            dataGridView1.DataSource = null;
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns["dbName"].Visible = false;
+            dataGridView1.Columns["dbNumber"].Visible = false;
+            dataGridView1.Columns["Name1"].Visible = true;
+            dataGridView1.Columns["Number"].Visible = true;
+            saveButton.Enabled = true;
+            dataGridView1.Refresh();
         }
 
         private void editToolStripMenuItem_Click(object sender, EventArgs e)
@@ -34,7 +50,36 @@ namespace Phonebook
 
         private void saveButton_Click(object sender, EventArgs e)
         {
+            Phone[] newNumber = new Phone[dataGridView1.RowCount - 1];
+            for (int i = 0; i < newNumber.Length; i++)
+            {
+                newNumber[i] = new Phone();
+                newNumber[i].Name = dataGridView1[3, i].Value.ToString();
+                newNumber[i].Number = dataGridView1[4, i].Value.ToString();
+                db.Phones.InsertOnSubmit(newNumber[i]);
+            }
+            db.SubmitChanges();
+            dataGridView1.Rows.Clear();
+            dataGridView1.Refresh();
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'database1DataSet.Table' table. You can move, or remove it, as needed.
+            this.tableTableAdapter.Fill(this.database1DataSet.Table);
+
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string search = textBox1.Text;
+
+            var query = from phone in db.Phones
+                        where phone.Name.Contains(search) || phone.Number.Contains(search)
+                        select phone;
+
+            dataGridView1.DataSource = query;
+            dataGridView1.Refresh();
         }
     }
 }
